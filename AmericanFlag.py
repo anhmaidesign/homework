fd29b63b864617ce69bfdc2a38f9e183729e2d08
Anh Mai <maihoaianh0410@gmail.com>
2017-07-27 06:07:15 +0000

    1950 flag

---
+++ AmericanFlag.py
@@ -0,0 +1,14 @@
+import sys
+for mainFlag in range(4):    
+    for flag in range(5):
+        sys.stdout.write('*')
+    for flag in range(10):
+        sys.stdout.write('=')
+    sys.stdout.write('\n') 
+for line in range(4):
+    for line2 in range(15):
+        sys.stdout.write('=')
+    sys.stdout.write('\n')
+    
+
+    
